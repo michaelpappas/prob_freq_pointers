5f095918ab4ff28981d31378b27210cb74c0df8a
michaelpappas <pappas.michael@gmail.com>
2022-11-11 16:06:40 -0800

    finished two first problems

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,42 @@
+"use strict"
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+
+
+//returning a boolean
+//inputs are an sorted array of numbers and a number
+//determine if there is a pair of numbers where the average of the two is the target number
+//initialize two variables, left is equal to the first index and right is equal to the last index
+//right a while statement where left is less than right
+//establish a consant called avg of the values where the pointers are pointing
+//if the avg is equal to our targetAvg then return true
+//else if the avg is less than the targetAvg increment left pointer by 1
+// else decrement right pointer by 1
+//return false
+
+
+//edge case: if the lenght of the input array is less than two return false
+
+function averagePair(nums, targetAvg) {
+  if(nums.length < 2){
+    return false
+  }
+
+  let left = 0;
+  let right = nums.length-1;
+
+  while(left < right){
+    const avg = (nums[left] + nums[right]) / 2
+    if(avg === targetAvg){
+      return true
+    }
+    else if(avg < targetAvg){
+      left++;
+    }
+    else{
+      right--;
+    }
+  }
+return false
+
 }
 
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,41 @@
+"use strict"
+//return a boolean
+//accepting two strings
+// string1 = word
+// string2 = letters
+//
+// fail fast - if word.length < letters.length -> fail
+// make a freqCounter for both strings and compare them
+// iterate through wordFreq to lettersFreq and see if the contents of wordFreq are inside lettersFreq
+// if wordFreq[char] is > than letterFreq then return false.
+// return true
+
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+
+/**  determins if the word string can be constructed from the letters string */
+function canConstructWord(word, letters) {
+  if(letters.length < word.length){
+    return false
+  }
+
+  const wordFreqs = getFrequencyCounter(word);
+  const lettersFreqs = getFrequencyCounter(letters);
+
+  for(let char in wordFreqs){
+    if(wordFreqs[char] > lettersFreqs[char] || !lettersFreqs[char]){
+      return false
+    }
+  }
+  return true
 }
+
+
+/** creates frequency counter given an array like input. */
+function getFrequencyCounter(items) {
+  const freqs = {};
+  for (const item of items) {
+  const curr = freqs[item] || 0;
+  freqs[item] = curr + 1;
+  }
+  return freqs;
+  }
