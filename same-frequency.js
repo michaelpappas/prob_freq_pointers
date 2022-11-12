b6b730e502a35eceac468b6974961de948779605
michaelpappas <pappas.michael@gmail.com>
2022-11-11 16:59:50 -0800

    finished samefreq and part of separate-positive

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,39 @@
-// add whatever parameters you deem necessary & write docstring
-function sameFrequency() {
+"use strict"
+
+//accepts two positive integers
+// returning a boolean if the two integers have the same frequency of digits
+//convert the two integers to strings
+//make a frequency counter of boths string
+//compare both frequency counters to see if their keys/values are the same
+//if they are not the same then return false
+//return true after successfully comparing
+
+
+
+/** check if two numbers have the same frequency of digits.**/
+function sameFrequency(num1, num2) {
+  const num1Str = num1.toString();
+  const num2Str = num2.toString();
+
+
+  const num1Freq = getFrequencyCounter(num1Str);
+  const num2Freq = getFrequencyCounter(num2Str);
+
+  for(let num in num1){
+    if(num1[num] !== num2[num]){
+      return false
+    }
+  }
+  return true
+}
+
+
+/** creates frequency counter given an array like input. */
+function getFrequencyCounter(items) {
+  const freqs = {};
+  for (const item of items) {
+    const curr = freqs[item] || 0;
+    freqs[item] = curr + 1;
+  }
+  return freqs;
 }
--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,25 @@
+
+"use strict"
+
+//input is an array of non-zero numbers
+//returns the same array of negative numbers on the right and positive numbers on the left
+//establish left and right pointers
+// left will equal 0, and right will equal the last index
+//while left is less than right
+//establish
+//Condition1
+//Left and right wrong, swap values
+
+//Condition4
+//Both are correct, left++ right—
+
+// Condition2
+// Left is correct but right is wrong, increment left++
+
+// Condition3
+// Left is wrong but right is correct, increment right—
+
 // add whatever parameters you deem necessary & write docstring
-function separatePositive() {
+function separatePositive(nums) {
+
 }
